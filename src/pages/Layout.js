e542ec3b4e62598311c49d137b1df8543352b7ac
Michael Olatunji <88350401+hollamik@users.noreply.github.com>
2022-08-16 17:00:37 +0100

    Update Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -13,7 +13,7 @@ function Layout() {
 <nav className="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
 <div className="container">
 <h3 className='logo'>Mascota.</h3> 
-<button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" onClick={()=>{updateExpanded(expand? true:"expand");}}></span>
+<button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span>
 </button>
 <div className="collapse navbar-collapse" id="navbarSupportedContent">
 <ul className="navbar-nav mx-auto">
